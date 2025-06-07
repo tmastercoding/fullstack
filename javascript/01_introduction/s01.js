cd5ae25adbc193d3b85e72cfe3cec56dd6a10b1c
tmastercoding <tmastercubing@gmail.com>
2025-06-07 13:11:46 +0800

    modify: js intro

---
+++ javascript/01_introduction/s01.js
@@ -0,0 +1,10 @@
+(function () {
+  "use strict";
+  /* Start of your code */
+  function greetMe(yourName) {
+    console.log(`Hello ${yourName}`);
+  }
+
+  greetMe("World");
+  /* End of your code */
+})();
